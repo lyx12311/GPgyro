6edd2d1b9946e69dc289dbd6920c4ec3e69b8d64
Soichiro Hattori <soichiro.hattori@gmail.com>
2024-03-05 14:40:16 -0500

    minimum package

---
+++ src/GPgyro/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+            "fitpoints",
+            "mean_function_both",
+            "build_gp",
+            "mean_function_both_FC",
+            "build_gp_FC",
+            "GP_gyro_PC",
+            "GP_gyro_FC",
+            "GP_gyro"
+            ]
+
+from .gpgyro import *
